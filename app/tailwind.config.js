b8b0476e803acb915388cedf7c5f42e5b5a7bff7
lkmdev01 <lkmdev01@gmail.com>
2024-11-02 19:23:24 -0300

    first commit

--- app/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-}
